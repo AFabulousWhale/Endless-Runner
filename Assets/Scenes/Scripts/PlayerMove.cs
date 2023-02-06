c23d375a772dbb273c77a7046cc74dd5b1b87fdc
AFabulousWhale <maraltom@gmail.com>
2023-02-06 15:58:32 +0000

    Added shark, if you hit an obstacle the shark goes forwards, if you hit another one whilst he is there then you die

--- Assets/Scenes/Scripts/PlayerMove.cs
+++ Assets/Scenes/Scripts/PlayerMove.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public GameObject deathCanvas, platforms;
+    public GameObject deathCanvas, platforms, Shark;
     public Score scoreScript;
+
+    bool sharkStartMoved = true;
+    int hitObstacleTimes = 0;
+    public Vector3 sharkForwardPos, sharkBackwardsPos, moveToPos;
+    public float timer;
     // Update is called once per frame
+
+    private void Start()
+    {
+        sharkForwardPos = Shark.transform.position;
+        sharkBackwardsPos = new Vector3(Shark.transform.position.x, Shark.transform.position.y, Shark.transform.position.z - 1.5f);
+    }
+
     void Update()
     {
+        timer += 0.5f;
         float speed = Input.GetAxis("Horizontal") * 10f;
         this.transform.Translate(speed*Time.deltaTime, 0, 0);
+
+        SharkCheck();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,7 +39,14 @@ public class PlayerMove : MonoBehaviour
             }
             else
             {
-                Stutter();
+                if(hitObstacleTimes == 2) //if you hit an obstacle on the side twice (the shark is forwards and you hit an obstacle again)
+                {
+                    Death();
+                }
+                else
+                {
+                    Stutter();
+                }
             }
         }
         if (other.tag == "Collectible")
@@ -37,13 +59,13 @@ public class PlayerMove : MonoBehaviour
     {
         Destroy(collectible);
         scoreScript.score += 100;
-
-        Debug.Log("collect");
     }
 
     void Stutter()
     {
-
+        hitObstacleTimes++;
+        sharkStartMoved = false;
+        MoveShark("forwards"); //moves the shark forwards if you've stuttered
     }
 
     void Death()
@@ -57,6 +79,46 @@ public class PlayerMove : MonoBehaviour
                 item.GetComponent<FloorController>().enabled = false;
             }
         }
+        scoreScript.enabled = false;
         this.enabled = false;
+    }
+
+    //shark movement depending on the player hitting the obstacles:
+
+    void SharkCheck()
+    {
+        if (timer == 200) //moves the shark backwards after 5 seconds at the start of the round
+        {
+            hitObstacleTimes = 0; //resets the obstacles hit
+            sharkStartMoved = false;
+            MoveShark("backwards");
+        }
+
+        if (!sharkStartMoved)
+        {
+            Shark.transform.position = Vector3.MoveTowards(Shark.transform.position, moveToPos, 0.05f);
+            if (Shark.transform.position == moveToPos)
+            {
+                //if the shark is facing forwards and has reached the destination then the timer will be reset but if the player hits another obstacle in that time then they die
+                if (moveToPos == sharkForwardPos)
+                {
+                    timer = 0;
+                }
+                sharkStartMoved = true; //boolean is used to make sure the shark moving is called once it reached it's desired position
+            }
+        }
+    }
+
+    void MoveShark(string direction)
+    {
+        switch(direction)
+        {
+            case "forwards":
+                moveToPos = sharkForwardPos;
+                break;
+            case "backwards":
+                moveToPos = sharkBackwardsPos;
+                break;
+        }
     }
 }
