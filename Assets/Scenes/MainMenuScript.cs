d76290cee530934245d0db2e99ec5be2aedf2190
ANGLIA\mm2297 <mm2297@student.aru.ac.uk>
2023-02-14 11:33:41 +0000

    Main menu basics are set up

---
+++ Assets/Scenes/MainMenuScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //go to main gameplay scene
+    }
+
+    public void Options()
+    {
+        //copy options from the pause menu
+        //change options to a seperate script
+    }
+
+    public void LeaderBoard()
+    {
+        //go to leaderboard scene
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
